195ab48e9a1ffac779de1d82004a9a1a9282772d
kevincanani <kevin.232122@alunosatc.edu.br>
2025-06-22 20:33:32 -0300

    visualizar

--- Screens/Visualizar.js
+++ Screens/Visualizar.js
@@ -1,9 +1,111 @@
-import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity, FlatList } from "react-native";
+import { useState, useEffect } from "react";
+import CardConsulta from "../components/cardConsulta";
+import { db } from "../controller";
+import { collection, doc, Firestore, getDocs } from 'firebase/firestore';
+import firebase from "firebase/compat/app";
+// import { useCarrinho } from "../CarrinhoProvider";
+// import Carrinho from "./Carrinho";
 
-export default function Veterinario() {
+export default function Visualizar() {
+
+    const DeletarBanhoTosa = async () => {
+            try {
+                Firestore()
+                .collection('veterinario')
+                .doc(id)
+                .delete()
+            } catch {
+                console.log('Erro ao cadastrar produto! ', error)
+            }
+        }
+
+    const [veterinario, setVeterinario] = useState([])
+    // const {adicionarProduto} = useCarrinho()
+
+    useEffect(() => {
+        async function carregarVeterinario() { //é assincrona pois espera uma resposta para ser executada
+            try{
+                const querySnapshot = await getDocs(collection(db, 'veterinario')) //será realizada após a variavel produtos ser buscada no banco
+                const lista = [];
+                querySnapshot.forEach((doc) => {
+                    lista.push({id:doc.id, ...doc.data()}); //adiciona todos os dados ao final da lista
+                });
+                setVeterinario(lista);
+            } catch(error){
+                console.log('Erro ao buscar o produto', error);
+            }
+        }
+        carregarVeterinario(); //exibe os produtos após a função ser criada
+    }, []);
+
+    const [banhotosa, setBanhoTosa] = useState([])
+
+    useEffect(() => {
+        async function carregarBanhoTosa() { //é assincrona pois espera uma resposta para ser executada
+            try{
+                const querySnapshot = await getDocs(collection(db, 'banhotosa')) //será realizada após a variavel produtos ser buscada no banco
+                const lista = [];
+                querySnapshot.forEach((doc) => {
+                    lista.push({id:doc.id, ...doc.data()}); //adiciona todos os dados ao final da lista
+                });
+                setBanhoTosa(lista);
+            } catch(error){
+                console.log('Erro ao buscar o produto', error);
+            }
+        }
+        carregarBanhoTosa(); //exibe os produtos após a função ser criada
+    }, []);
+
     return(
         <View style={styles.containerLogin}>
-            <Text style={styles.textTitle}>Veterinario</Text>
+            <Text style={styles.textTitle}>Suas consultas: </Text>
+
+            <Text>Consultas: </Text>
+
+            <FlatList
+            data={veterinario}
+            renderItem={({item}) => (
+                <CardConsulta
+                //id={item.id}
+                animal={item.animal}
+                raca={item.raca}
+                tipoconsulta={item.tipoconsulta}
+                data={item.data}
+                horario={item.horario}
+                // deletar={()=>{DeletarBanhoTosa(item)}}
+                // imagem={item.imagem}
+                // comprar={()=>{
+                //     adicionarProduto(item);
+                //     navigation.navigate(Carrinho);
+                // }}
+                />
+            )}
+            keyExtractor={item => item.id}
+            />
+
+            <Text>Banhos e Tosas: </Text>
+
+            <FlatList
+            data={banhotosa}
+            renderItem={({item}) => (
+                <CardConsulta
+                //id={item.id}
+                animal={item.animal}
+                raca={item.raca}
+                especificacoes={item.especificacoes}
+                data={item.data}
+                horario={item.horario}
+                // deletar={()=>{DeletarBanhoTosa(item)}}
+                // imagem={item.imagem}
+                // comprar={()=>{
+                //     adicionarProduto(item);
+                //     navigation.navigate(Carrinho);
+                // }}
+                />
+            )}
+            keyExtractor={item => item.id}
+            />
 
             <TouchableOpacity>
                 <Text style={styles.txtBtn}>Alterar</Text>
@@ -20,17 +122,19 @@ const styles = StyleSheet.create({
         color: '#FFFFFF'
     },
     txtInput:{
-        width: 300,
+        fontWeight: 'bold',
+        width: 325,
         borderWidth: 2,
         borderColor: '#7B00FF',
-        borderRadius: 10,
-        padding: 10,
+        borderRadius: 15,
+        padding: 15,
         alignSelf: 'center',
-        margin: 12,
+        margin: 15,
         backgroundColor: '#00D8CD'
     },
     textTitle:{
-        fontSize: 30,
+        padding: 25,
+        fontSize: 45,
         fontWeight: 'bold',
         alignSelf: 'center'
     },
